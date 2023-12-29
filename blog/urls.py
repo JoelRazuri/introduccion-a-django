0454dad715c4bd5ce392456e0fa86895ca8f522f
Joel Razuri <jrazuri98@hotmail.com>
2023-12-29 19:56:29 -0300

    forms-update

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
-from .views import BlogListView, BlogCreateView, BlogDetailView
+from .views import BlogListView, BlogCreateView, BlogDetailView, BlogUpdateView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
-    path("create/", BlogCreateView.as_view(), name="create"),
-    path("<int:pk>/", BlogDetailView.as_view(), name="detail"),
+    path('create/', BlogCreateView.as_view(), name='create'),
+    path('<int:pk>/', BlogDetailView.as_view(), name='detail'),
+    path('<int:pk>/update/', BlogUpdateView.as_view(), name='update'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect, get_object_or_404
-from django.views.generic import View
+from django.views.generic import View, UpdateView
+from django.urls import reverse_lazy
 from .forms import PostCreateForm
 from .models import Post
 
@@ -52,3 +53,14 @@ class BlogDetailView(View):
         }
         
         return render(request, 'blog_detail.html', context)
+    
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    fields = ['title', 'content']
+    template_name = 'blog_update.html'
+
+    def get_success_url(self):
+        pk = self.kwargs['pk']
+
+        return reverse_lazy('blog:detail', kwargs={'pk':pk})
