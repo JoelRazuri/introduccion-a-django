ee5f4cc7852030d49a75c68cd987ffb31f9e875d
Joel Razuri <jrazuri98@hotmail.com>
2023-12-29 19:19:14 -0300

    forms-read

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import BlogListView, BlogCreateView
+from .views import BlogListView, BlogCreateView, BlogDetailView
 
 app_name = 'blog'
 
 urlpatterns = [
     path('', BlogListView.as_view(), name='home'),
-    path("create/", BlogCreateView.as_view(), name="create")
+    path("create/", BlogCreateView.as_view(), name="create"),
+    path("<int:pk>/", BlogDetailView.as_view(), name="detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,20 +1,22 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from django.views.generic import View
-from .forms import PostCreateFrom
+from .forms import PostCreateForm
 from .models import Post
 
 
 class BlogListView(View):
     def get(self, request, *args, **kwargs):
+        posts = Post.objects.all()
+
         context = {
-            
+            'posts': posts
         }
         return render(request, 'blog_list.html', context)
 
 
 class BlogCreateView(View):
     def get(self, request, *args, **kwargs):
-        form = PostCreateFrom
+        form = PostCreateForm
         context = {
             'form': form
         }
@@ -23,7 +25,7 @@ class BlogCreateView(View):
     
     def post(self, request, *args, **kwargs):
         if request.method == "POST":
-            form = PostCreateFrom(request.POST)
+            form = PostCreateForm(request.POST)
             
             if form.is_valid():
                 title = form.cleaned_data.get('title')
@@ -39,3 +41,14 @@ class BlogCreateView(View):
         }
 
         return render(request, "blog_create.html", context)
+    
+
+class BlogDetailView(View):
+    def get(self, request, pk,*args, **kwargs):
+        post = get_object_or_404(Post, pk=pk)
+        
+        context = {
+            'post': post
+        }
+        
+        return render(request, 'blog_detail.html', context)
