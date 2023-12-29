96030d09a406cf66fbc667b4fd28e1957e52c66d
Joel Razuri <jrazuri98@hotmail.com>
2023-12-29 18:59:14 -0300

    forms-create

--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic import View
 from .forms import PostCreateFrom
+from .models import Post
 
 
 class BlogListView(View):
@@ -13,13 +14,26 @@ class BlogListView(View):
 
 class BlogCreateView(View):
     def get(self, request, *args, **kwargs):
+        form = PostCreateFrom
         context = {
-
+            'form': form
         }
 
         return render(request, "blog_create.html", context)
     
     def post(self, request, *args, **kwargs):
+        if request.method == "POST":
+            form = PostCreateFrom(request.POST)
+            
+            if form.is_valid():
+                title = form.cleaned_data.get('title')
+                content = form.cleaned_data.get('content')
+
+                p, created = Post.objects.get_or_create(title=title, content=content)
+                p.save()
+
+                return redirect('blog:home')
+        
         context = {
 
         }
